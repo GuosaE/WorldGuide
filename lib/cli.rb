2aa3a748a47c9be4e2bb2adef7fb9fa7c8d4293d
Guosa_Edebiri <gedebiri@gmail.com>
2021-10-21 20:29:51 -0500

    added run method to CLI class

--- lib/cli.rb
+++ lib/cli.rb
@@ -6,4 +6,15 @@ class CommandLineInterface
         puts "Welcome to Geographica, the best resource on the distribution of languages in countries around the globe!"
     end
 
+    def run
+        greet
+        puts "To see a list of all regions please enter 'Regions'."
+        puts "To see a list of all countries, please enter 'Countries'."
+        puts "To see a list of all national languages, please enter, 'Languages'."
+        puts "To exit the application, please type 'exit'."
+        input = gets.chomp
+        if input == 'exit' || input == 'Exit'
+            'exit'
+    end
+
 end
