57cf764f56e1b36bdf77df67f885e751d356e14f
Guosa_Edebiri <gedebiri@gmail.com>
2021-10-21 20:47:08 -0500

    added get country data method to country class

--- lib/country.rb
+++ lib/country.rb
@@ -1,4 +1,13 @@
+require 'net/http'
+require 'open-uri'
+require 'json'
+require_relative 'region.rb'
+require_relative 'language.rb'
+require 'pry'
+
 class Country
+
+    URL = "https://restcountries.com/v3/all"
 
     attr_accessor :name, :region, :language
 
@@ -32,6 +41,12 @@ class Country
 
     def region=(region)
         @region = region
+    end
+
+    def self.get_country_data
+        uri = URI.parse(URL)
+        response = Net::HTTP.get_response(uri)
+        response.body
     end
 
 
