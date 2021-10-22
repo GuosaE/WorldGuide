94abee17e8103a3c54f1d2848ce489a900a537da
Guosa_Edebiri <gedebiri@gmail.com>
2021-10-21 20:28:08 -0500

    added greet method to CLI class

---
+++ lib/cli.rb
@@ -0,0 +1,9 @@
+require 'pry'
+
+class CommandLineInterface
+
+    def greet
+        puts "Welcome to Geographica, the best resource on the distribution of languages in countries around the globe!"
+    end
+
+end
