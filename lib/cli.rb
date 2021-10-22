e0e73d7c0dca88d9adc24e4a265db0b8f84f10b6
Guosa_Edebiri <gedebiri@gmail.com>
2021-10-21 21:04:15 -0500

    had old name of project in the greeting message, corrected it to final name of project

--- lib/cli.rb
+++ lib/cli.rb
@@ -3,7 +3,7 @@ require 'pry'
 class CommandLineInterface
 
     def greet
-        puts "Welcome to Geographica, the best resource on the distribution of languages in countries around the globe!"
+        puts "Welcome to WorldGuide the best resource on the distribution of languages in countries around the globe!"
     end
 
     def run
